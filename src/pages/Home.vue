7c3d30ea527ba7c26f7eb7074089f2ad8f7b072f
Rom-Freedom <romeo.dark@mail.ru>
2023-11-14 13:55:08 +0300

    set up Pinia, to get lists from server

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,31 @@
 <script setup>
 import AppLayout from '../components/App.Layout.vue'
+import { useRootStore } from '@/stores/root'
+
+const rootStore = useRootStore();
+rootStore.getIngredients();
 </script>
 
 <template>
-    <AppLayout imgUrl="/src/assets/img/bg-1.jpg" />
+    <AppLayout imgUrl="/src/assets/img/bg-1.jpg">
+        <div class="wrapper">
+            <div class="info">
+                <div class="title">Choose your drink</div>
+                <div class="line"></div>
+            </div>
+        </div>
+    </AppLayout>
 </template>
 
 <style lang="sass" scoped>
 @import '../assets/styles/main'
+
+.wrapper
+    display: flex
+    align-items: center
+    justify-content: center
+
+.info
+    padding: 80px 0
+    text-align: center
 </style>
