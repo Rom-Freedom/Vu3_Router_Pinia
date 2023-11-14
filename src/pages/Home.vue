4d1dbe19836d74e58e1e89dad734faa4ae7c6ffb
Rom-Freedom <romeo.dark@mail.ru>
2023-11-14 15:28:01 +0300

    to style select with Element Plus

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,14 @@
 <script setup>
+import { ref } from 'vue';
 import AppLayout from '../components/App.Layout.vue'
 import { useRootStore } from '@/stores/root'
+import { storeToRefs } from 'pinia';
 
 const rootStore = useRootStore();
 rootStore.getIngredients();
+
+const {ingredients} = storeToRefs(rootStore);
+const ingredient = ref(null);
 </script>
 
 <template>
@@ -12,6 +17,25 @@ rootStore.getIngredients();
             <div class="info">
                 <div class="title">Choose your drink</div>
                 <div class="line"></div>
+                <div class="select-wrapper">
+                    <el-select
+                        v-model="ingredient"  
+                        placeholder="Choose main ingredient" 
+                        size="large"
+                        class="select"
+                    >
+                        <el-option
+                        v-for="item in ingredients"
+                        :key="item.strIngredient1"
+                        :label="item.strIngredient1"
+                        :value="item.strIngredient1"
+                        />
+                    </el-select>
+                </div>
+                <div class="text">
+                    Try our delicious cocktail recipes for every occasion. Find delicious cocktail recipes by ingredient through our cocktail generator.
+                </div>
+                <img src="/src/assets/img/cocktails.png" alt="Cocktails" class="img">
             </div>
         </div>
     </AppLayout>
@@ -28,4 +52,21 @@ rootStore.getIngredients();
 .info
     padding: 80px 0
     text-align: center
+
+.select-wrapper
+    padding-top: 50px
+
+.select
+    width: 220px
+
+.text
+    max-width: 516px
+    margin: 0
+    padding-top: 50px
+    line-height: 36px
+    letter-spacing: 0.1em
+    color: $textMuted
+
+.img
+    margin-top: 60px
 </style>
